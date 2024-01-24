32405130fda1885761a7df1e20439c182dd14541
DMKALALA <96592192+DMKALALA@users.noreply.github.com>
2024-01-24 01:31:28 -0500

    Added the finished Part04 and the new Part05

--- Part1/Part04/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
+++ Part1/Part04/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -9,10 +9,18 @@ public class GuestListFromAFile {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Name of the file:");
-        String file = scanner.nextLine();
+        String file = SafeInput.getNonZeroLenString(scanner, "Name of the file:");
 
         ArrayList<String> list = new ArrayList<>();
         // implement reading the file here.
+        try(Scanner read = new Scanner(Paths.get(file))) {
+            while (read.hasNextLine()) {
+                list.add(read.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+            
+        }
         System.out.println("");
 
         System.out.println("Enter names, an empty line quits.");
